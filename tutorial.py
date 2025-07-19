1c578be5fe58aa0524b2254d6c27050366112952
ChinmayShukla04 <chinmayshukla04@gmail.com>
2025-07-20 00:08:06 +0530

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -75,7 +75,8 @@ with open(ANNOTATION_PATH + '\label_map.pbtxt', 'w') as f:
 """# 2. Create TF records"""
 
 import subprocess  
-command = f"python {SCRIPTS_PATH}/generate_tfrecord.py -x {IMAGE_PATH}/train -l {ANNOTATION_PATH}/label_map.pbtxt -o {ANNOTATION_PATH}/train.record" subprocess.run(command, shell=True, check=True)
+command = f"python {SCRIPTS_PATH}/generate_tfrecord.py -x {IMAGE_PATH}/train -l {ANNOTATION_PATH}/label_map.pbtxt -o {ANNOTATION_PATH}/train.record" 
+subprocess.run(command, shell=True, check=True)
 !python {SCRIPTS_PATH + '/generate_tfrecord.py'} -x{IMAGE_PATH + '/test'} -l {ANNOTATION_PATH + '/label_map.pbtxt'} -o {ANNOTATION_PATH + '/test.record'}
 
 """# 3. Download TF Models Pretrained Models from Tensorflow Model Zoo"""
